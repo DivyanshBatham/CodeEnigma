1ac57e75ff0ad7dab51a69761fb6300640f60453
Divyansh Batham <divyansh.sunita@gmail.com>
2017-09-09 22:28:50 +0530

    CodeFixed

--- routes/login.js
+++ routes/login.js
@@ -10,24 +10,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  // var MongoClient = mongodb.MongoClient;
-  // var url = 'mongodb://localhost:27017/enigmadb';
-  // MongoClient.connect(url,function(err,db){
-  // 	if(err)
-  // 		console.log('Unable to connect to mongodb', err);
-  // 	else
-  // 	{
-  // 		console.log('connection established');
-  //
-  // 		var users = db.collection('users');
-  // 		users.find({}).toArray(function(err,result){
-  // 			if(err)
-  // 				res.send(err);
-  // 			else if(result.length)
-  // 				console.log(result);
-  // 		})
-  // 	}
-  // })
   console.log(req.body.TeamId,req.body.Password);
   mongoose.model('users').findOne( { id:req.body.TeamId, pass:req.body.Password } ,function(err,users){
     res.send(users);
