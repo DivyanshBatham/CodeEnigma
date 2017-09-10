6a8a58b666aafc27afbb024a3e1d508d15e34565
Divyansh Batham <divyansh.sunita@gmail.com>
2017-09-10 23:36:09 +0530

    Dashboard Cols Created

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('code', { title: 'Code' });
+  res.render('home', { title: 'Home' });
 });
 
 module.exports = router;
